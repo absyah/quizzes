d5f433b5c61a4f264f837b64a1ca4e8428a97808
absyah <ardian.bahtiarsyah@gmail.com>
2024-12-22 22:15:36 +0700

    update contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-
 export default async function Home() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        <h2 className="font-medium text-xl mb-4">Welcome to Quiz Time</h2>
       </main>
     </>
   );
