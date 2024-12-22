af0d61e7940bea4cc53ac2bbc7f5843617d3d145
absyah <ardian.bahtiarsyah@gmail.com>
2024-12-22 14:33:36 +0700

    add link navigations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
-import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import PointButton from "@/components/point-button";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -43,9 +42,10 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
+                    <Link href={"/quizzes"}>Quiz Time</Link>
+                    <div className="flex items-center gap-2 font-normal">
+                      <Link href={"/quizzes"}>Quizzes</Link>
+                      <Link href={"/leaderboard"}>Leaderboard</Link>
                     </div>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
